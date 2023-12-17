f7351df89abdbc19892d6bf2476d661e33641f2b
Xamelo <100118522+Xamelo@users.noreply.github.com>
2023-12-17 20:40:08 +0100

    Update zad5.php

--- 08_12_2023/zad5.php
+++ 08_12_2023/zad5.php
@@ -1,3 +1,42 @@
 <?php
-    
+    function funkcja($x)
+    {
+        return $x**2 - 4;;
+    }
+
+    function metodaPolowienia($funkcja, $a, $b, $epsilon)
+    {
+        if ($funkcja($a) * $funkcja($b) >= 0)
+        {
+            echo "Błąd: Funkcja nie spełnia warunków wymaganych dla metody połowienia.";
+            return;
+        }
+
+        $c = $a;
+        while (($b - $a) >= $epsilon)
+        {
+            $c = ($a + $b) / 2;
+            if (abs($funkcja($c)) < $epsilon)
+            {
+                break;
+            }
+
+            if ($funkcja($c) * $funkcja($a) < 0)
+            {
+                $b = $c;
+            }
+            else
+            {
+                $a = $c;
+            }
+        }
+
+        echo "Przybliżone miejsce zerowe: " . $c;
+    }
+
+    $a = 5;
+    $b = 20;
+    $epsilon = 1;
+
+    metodaPolowienia('funkcja', $a, $b, $epsilon);
 ?>
