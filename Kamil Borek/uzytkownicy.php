d67a2d4386cfd7bc4a25ce6f79fbd28a88816d4d
Xamelo <100118522+Xamelo@users.noreply.github.com>
2024-06-10 19:43:19 +0200

    Update uzytkownicy.php

--- Kamil Borek/uzytkownicy.php
+++ Kamil Borek/uzytkownicy.php
@@ -1,92 +1,81 @@
-<?php
-$connection = mysqli_connect("localhost", "root", "", "portal");
-
-$ask = "SELECT COUNT(*) FROM dane";
-$result = mysqli_query($connection, $ask);
-$resultText = mysqli_fetch_array($result);
-?>
-
+<!DOCTYPE html>
 <html lang="pl-PL">
-    <head>
-        <title>Portal społecznościowy</title>
-        <link rel="stylesheet" href="styl5.css"/>
-    </head>
-    <body>
-        <header>
-            <div class="leftBanner">
-                <h2>Nasze osiedle</h2>
+<head>
+    <meta charset="UTF-8"/>
+    <title>Portal społecznościowy</title>
+    <link rel="stylesheet" href="styl5.css"/>
+</head>
+<body>
+    <header>
+        <section class="prawy">
+            <h2>Nasze osiedle</h2>
+        </section>
+        <section class="lewy">
+            <?php
+                $id = mysqli_connect("localhost", "root", "", "portal");
+                $q = "SELECT COUNT(*) FROM dane;";
+                $result = mysqli_query($id, $q);
+                echo "<h5>"."Liczba użytkowników portalu:".mysqli_fetch_row($result)[0]."</h5>";
+                mysqli_close($id);
+            ?>
+        </section>
+    </header>
+    <main>
+        <section class="lewy">
+            <h3>Logowanie</h3>
+            <form action="uzytkownicy.php" method="post">
+                <label for="login">login</label><br/>
+                <input type="text" name="login" id="login"/><br/>
+                <label for="haslo">hasło</label><br/>
+                <input type="password" name="haslo" id="haslo"/><br/>
+                <input type="submit" value="Zaloguj"/>
+            </form>
+        </section>
+        <section class="prawy">
+            <h3>Wizytówka</h3>
+            <div class="wizytowka">
+                <?php
+                    skrypt2();
+                ?>
             </div>
-            <div class="rightBanner">
-                <?php echo "<h5>Liczba użytkowników portalu " . $resultText[0] . "</h5>"; mysqli_close($connection);?>
-            </div>
-        </header>
-        <main>
-            <div class="leftMain">
-                <h3>Logowanie</h3>
-                <form action="uzytkownicy.php" method="post">
-                    <label>
-                        login</br>
-                        <input type="text" name="login" id="login"/></br>
-                        hasło</br>
-                        <input type="text" name="password" id="login"/></br>
-                        <input type="submit" value="Zaloguj"/>
-                    </label>
-                </form>
-            </div>
-            <div class="rightMain">
-                <h3>Wizytówka</h3>
-                <div class="card">
-                    <?php
-                    $connection = mysqli_connect("localhost", "root", "", "portal");
-                    if (isset($_POST['login']) && isset($_POST['password']))
-                    {
-                        $login = $_POST['login'];
-                        $password = $_POST['password'];
-                        if ($login != "" && $password != "")
-                        {
-                            $passwordHashed = sha1($password);
-
-                            $question = "SELECT * FROM uzytkownicy WHERE login = \"$login\"";
-                            $result2 = mysqli_query($connection, $question);
-                            if (mysqli_num_rows($result2) == 0)
-                            {
-                                echo "Login nie istnieje";
-                            }
-                            else
-                            {
-                                $row = mysqli_fetch_assoc($result2);
-                                if ($password == $row['haslo'])
-                                {
-                                    $question = "SELECT `uzytkownicy`.`login`, `dane`.`rok_urodz`, `dane`.`przyjaciol`, `dane`.`hobby`, `dane`.`zdjecie` FROM `uzytkownicy`, `dane`
-                                    WHERE `uzytkownicy`.`login` = \"$login\"";
-                                    $result2 = mysqli_query($connection, $question);
-                                    $row = mysqli_fetch_assoc($result2);
-                                    $rokUrodz = $row['rok_urodz'];
-                                    $przyjaciol = $row['przyjaciol'];
-                                    $hobby = $row['hobby'];
-                                    $zdjecie = $row['zdjecie'];
-                                    echo "<img src=\"$zdjecie\" alt=\"osoba\">";
-                                    $wiek = 2024 - $rokUrodz;
-                                    echo "<h4>$login ($wiek)</h4>";
-                                    echo "<p>hobby: $hobby</p>";
-                                    echo "<h1><img src=\"icon-ong.png\">$przyjaciol</h1>";
-                                    echo "<a href=\"dane.html\">";
-                                    echo "<button>Wiecej informacji</button>";
-                                    echo "</a>";
-                                }
-                                else
-                                {
-                                    echo "Hasło nieprawidłowe";
-                                }
-                            }
-                        }
-                    }
-                    ?>
-                </div>
-            </div>
-        </main>
-        <footer>
-            Stronę wykonał: Kamil Borek
-        </footer>
-    </body>
+        </section>
+    </main>
+    <footer>
+        Stronę wykonał: 0000000
+    </footer>
+</body>
 </html>
+<?php
+    function skrypt2()
+    {
+        $id = mysqli_connect("localhost", "root", "", "portal");
+        if (!empty($_POST['login']) || !empty($_POST['haslo'])) {
+            $login = $_POST['login'];
+            $haslo = $_POST['haslo'];
+            $q = "SELECT haslo FROM uzytkownicy WHERE uzytkownicy.login = '$login';";
+            $result = mysqli_query($id, $q);
+
+            if (mysqli_num_rows($result) != 1) {
+                echo "login nie istnieje";
+                return;
+            } else {
+                if (sha1($haslo) != mysqli_fetch_row($result)[0]) {
+                    echo "hasło nieprawidłowe";
+                } else {
+                    $q2 = "SELECT uzytkownicy.login, dane.rok_urodz, dane.przyjaciol, dane.hobby, dane.zdjecie FROM
+                     uzytkownicy JOIN dane ON uzytkownicy.id = dane.id WHERE uzytkownicy.login = '$login';";
+                    $result2 = mysqli_query($id, $q2);
+                    $row = mysqli_fetch_array($result2);
+                    $wiek = 2024 - $row['rok_urodz'];
+                    echo "<img src=".$row['zdjecie']." alt='osoba'/><br/>";
+                    echo "<h4>".$login." (".$wiek.")</h4>";
+                    echo "<h1><img src='icon-on.png'/>".$row['przyjaciol']."</h4>";
+                    echo "<a href='dane.html'><button>Więcej informacji</button>";
+                }
+            }
+        } else {
+            return;
+        }
+        mysqli_close($id);
+    }
+?>
